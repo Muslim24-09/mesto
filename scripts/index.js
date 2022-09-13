61de19e0ff159fe0d7a8ed70ce0b063426b70c5f
Muslim24-09 <Leonard76@mail.ru>
2022-09-14 01:23:17 +0300

    sixth work review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 const popup = document.querySelector('.popup')
 const popupProfile = document.querySelector('#popup-profile')
-const editBtn = document.querySelector('.profile__edit-button')
-const closeBtn = document.querySelector('.popup__close-button')
-const formSaveButton = document.querySelector('.form__save-button');
-const closeBtnAction = document.querySelector('.popup__close-button_action')
+const editorBtn = document.querySelector('.profile__edit-button')
+// const closeBtn = document.querySelector('.popup__close-button')
+// const formSaveButton = document.querySelector('.form__save-button');
+// const closeBtnAction = document.querySelector('.popup__close-button_action')
 const nameInput = document.querySelector('.form__input_type_name')
 const aboutInput = document.querySelector('.form__input_type_about')
 const formEditor = document.querySelector('.form')
@@ -12,27 +12,45 @@ const profileName = document.querySelector('.profile__name')
 const profileAbout = document.querySelector('.profile__about')
 const elements = document.querySelector('.elements')
 const popupAction = document.querySelector('#popup-action')
-const addBtn = document.querySelector('.profile__add-button')
-const closeBtnAct = document.querySelector('.popup__close-button_action')
+const addtBtn = document.querySelector('.profile__add-button')
+// const closeBtnAct = document.querySelector('.popup__close-button_action')
 const form = document.querySelector('.form')
 const titleInput = document.querySelector('.form__input_type_title')
 const linkInput = document.querySelector('.form__input_type_link')
-const editProfileButton = document.querySelector('.profile__edit-button');
-const addButton = document.querySelector('.profile__add-button');
+// const editProfileButton = document.querySelector('.profile__edit-button');
+// const addButton = document.querySelector('.profile__add-button');
 const popupPictures = document.querySelector('#pictures-popup')
 const picturesBtn = document.querySelector('.element__image')
-const closeBtnpic = document.querySelector('.popup__close-button_pictures')
+// const closeBtnpic = document.querySelector('.popup__close-button_pictures')
 const contPic = document.querySelector('.popup__container_pictures')
 const titlePic = document.querySelector('.popup__pictures-title')
 const pictures = document.querySelector('.popup__pictures')
 
 
+const formData = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  saveButtonSelector: '.form__save-button',
+  inactiveButtonClass: 'form__save-button_disabled',
+  inputErrorClass: 'form__input_type_error',
+}
+
+
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      closePopup(popup);
+    }
+  })
+
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+
+  restartFormState(popup, formData)
 }
 
 nameInput.defaultValue = profileName.textContent
@@ -40,25 +58,25 @@ aboutInput.defaultValue = profileAbout.textContent
 
 
 
-editBtn.addEventListener('click', function () {
+editorBtn.addEventListener('click', function () {
   openPopup(popupProfile)
   nameInput.value = profileName.textContent
   aboutInput.value = profileAbout.textContent
 })
 
-const closeButtons = document.querySelectorAll ('.popup__close-button')
-closeButtons.forEach((button) => {
-  const popup = button.closest('.popup');
-  button.addEventListener('click', () => closePopup(popup));
+// const popupCloseButtons = document.querySelectorAll ('.popup__close-button')
+// popupCloseButtons.forEach((button) => {
+//   const popup = button.closest('.popup');
+//   button.addEventListener('click', () => closePopup(popup));
 
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      closePopup(popup);
-    }
-  })
-});
+//   document.addEventListener('keydown', (event) => {
+//     if (event.key === 'Escape') {
+//       closePopup(popup);
+//     }
+//   })
+// });
 
-const closePopupClick = () => {
+const popupCloseClick = () => {
   const popupList = Array.from(document.querySelectorAll('.popup'));
   popupList.forEach((popup) => {
       popup.addEventListener('click', (evt) => {
@@ -68,7 +86,7 @@ const closePopupClick = () => {
       })
   })
 }
-closePopupClick();
+popupCloseClick();
 
 
 formEditor.addEventListener('submit', function (e) {
@@ -79,7 +97,7 @@ formEditor.addEventListener('submit', function (e) {
 })
 
 
-addBtn.addEventListener('click', function () {
+addtBtn.addEventListener('click', function () {
   openPopup(popupAction)
 })
 
@@ -93,6 +111,31 @@ function cardCreater(data) {
   cardImage.alt = `Фотография: ${data.name}`
   cardTitle.textContent = data.name;
 
+  cardElement.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('element__like-button')) {
+      target.classList.toggle('element__like-button_active');
+    }
+
+    if (target && target.classList.contains('element__delete-button')) {
+      const item = target.closest('.element')
+      item.remove()
+    }
+
+    if (target && target.classList.contains('element__image')) {
+      openPopup(popupPictures)
+
+
+      titlePic.textContent = data.name
+
+      pictures.src = data.link
+      pictures.alt = `Фотография: ${data.name}`
+    }
+
+  })
+
+
   return cardElement;
 }
 
@@ -104,6 +147,7 @@ formCreator.addEventListener('submit', function addCard(event) {
   }
 
   const card = cardCreater(myCard)
+
   elements.prepend(card)
   event.target.reset()
   closePopup(popupAction)
@@ -113,44 +157,4 @@ for (let i = 0; i < addingPictures.length; i += 1) {
   elements.append(cardCreater(addingPictures[i]))
 }
 
-elements.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target && target.classList.contains('element__like-button')) {
-    target.classList.toggle('element__like-button_active');
-  }
-})
-
-elements.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target && target.classList.contains('element__delete-button')) {
-    const item = target.closest('.element')
-    item.remove()
-  }
-})
-
-
-elements.addEventListener('click', function (event) {
-  const target = event.target
-
-  if (target && target.classList.contains('element__image')) {
-    openPopup(popupPictures)
-
-    const parent = event.target.closest('.element')
-    const title = parent.querySelector('.element__title')
-
-    titlePic.textContent = title.textContent
-
-    const picture = target.closest('.element__image')
-
-    pictures.src = picture.src
-    pictures.alt = `Фотография: ${title.textContent}`
-  }
-})
-
-enableValidation({
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  saveButtonSelector: '.form__save-button',
-  inactiveButtonClass: 'form__save-button_disabled',
-  inputErrorClass: 'form__input_type_error',
-});
+enableValidation(formData);
--- scripts/validate.js
+++ scripts/validate.js
@@ -7,8 +7,6 @@
 // });
 
 
-
-
 //Получаем массив всех форм
 function enableValidation(formData) {
   const formArr = Array.from(document.querySelectorAll(formData.formSelector))
@@ -17,18 +15,15 @@ function enableValidation(formData) {
     formArr[i].addEventListener('submit', (e) => {
       e.preventDefault()
     })
-
     connectAllEventListeners(formArr[i], formData.inputSelector, formData.saveButtonSelector,
       formData.inactiveButtonClass, formData.inputErrorClass)
   }
 }
 
+
 function connectAllEventListeners(form, formInput, saveButton, inactiveSaveButton, inputErrorClass) {
   const inputArr = Array.from(form.querySelectorAll(formInput))
   const saveBtn = form.querySelector(saveButton)
-
-  toggleSaveButtonStatus(inputArr, saveBtn, inactiveSaveButton)
-
 
   for (let i = 0; i < inputArr.length; i++) {
     inputArr[i].addEventListener('input', () => {
@@ -41,12 +36,12 @@ function connectAllEventListeners(form, formInput, saveButton, inactiveSaveButto
 
 // функция изменения состояния кнопки
 function toggleSaveButtonStatus(inputArr, saveButton, inactiveSaveButton) {
-  if (checkInputValidity(inputArr)) {
-    saveButton.disabled = false;
-    saveButton.classList.remove(inactiveSaveButton)
-  } else {
+  if (!checkInputValidity(inputArr)) {
     saveButton.disabled = true;
     saveButton.classList.add(inactiveSaveButton)
+  } else {
+    saveButton.disabled = false;
+    saveButton.classList.remove(inactiveSaveButton)
   }
 }
 
@@ -62,20 +57,64 @@ function checkInputValidity(inputArr) {
 function toggleInputError(form, inputElement, inputErrorClass) {
   const errorElement = form.querySelector(`.form__input-${inputElement.name}-error`)
 
+  if (!errorElement) {
+    return;
+  }
   if (inputElement.validity.valid) {
     hideError(inputElement, inputErrorClass, errorElement)
   } else {
-      showError(inputElement, inputErrorClass, errorElement)
+    showError(inputElement, inputErrorClass, errorElement)
   }
 }
 
 function showError(input, errorClass, errorElement) {
-      input.classList.add(errorClass)
-      errorElement.textContent=input.validationMessage
+  input.classList.add(errorClass)
+  errorElement.textContent = input.validationMessage
+}
+function hideError(input, errorClass, errorElement) {
+  input.classList.remove(errorClass)
+  errorElement.textContent = ''
 }
 
+//функция очистки полей и ошибок если в попапе есть форма
+function restartFormState(popup, formData) {
+  const saveBtn = popup.querySelector(formData.saveButtonSelector)
+  const form = popup.querySelector(formData.formSelector)
 
-function hideError (input, errorClass, errorElement) {
-  input.classList.remove(errorClass)
-  errorElement.textContent=''
+  if (popup.querySelector(formData.saveButtonSelector)) {
+    const errorList = Array.from(form.querySelectorAll('.form__error'))
+    const inputList = Array.from(form.querySelectorAll(formData.inputSelector))
+
+    restartError(errorList, inputList, formData)
+    restartForm(form)
+
+    toggleInputError(form, saveBtn, formData.inactiveButtonClass)
+  }
+}
+
+//функция сброса формы
+function restartForm(form) {
+  form.reset()
+}
+
+// функция сброса ошибок
+// function restartError(errorList, inputList, formData){
+//   errorList.forEach((errorElement) => {
+//     errorElement.textContent = "";
+
+// })
+
+// inputList.forEach((formInput) => {
+//     formInput.classList.remove(formData.inputErrorClass);
+// })
+// }
+
+// функция сброса ошибок
+function restartError(errorList, inputList, formData){
+  for (let i = 0; i < errorList.length; i++){
+    errorList[i].textContent = ""
+  }
+for (let i = 0; i < inputList.length; i++){
+    inputList[i].classList.remove(formData.inputErrorClass)
+  }
 }
